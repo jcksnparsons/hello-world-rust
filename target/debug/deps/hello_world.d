9e60c1433869c3efc0293e363a2e4b3f402ea766
Jack Parsons <jcksnparsons@yahoo.com>
2020-07-12 15:05:55 -0500

    built project optimized for release

---
+++ target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/jcksnparsons/coding-practice/learning_rust/hello_world/target/debug/deps/hello_world: src/main.rs
+
+/Users/jcksnparsons/coding-practice/learning_rust/hello_world/target/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/jcksnparsons/coding-practice/learning_rust/hello_world/target/release/hello_world: /Users/jcksnparsons/coding-practice/learning_rust/hello_world/src/main.rs
